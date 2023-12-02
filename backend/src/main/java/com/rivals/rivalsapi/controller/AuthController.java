d255b042c20722660a3669ea414cd7cce31f340b
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 20:07:36 +0200

    feat: add register & login endpoints in authentication controller

--- backend/src/main/java/com/rivals/rivalsapi/controller/AuthController.java
+++ backend/src/main/java/com/rivals/rivalsapi/controller/AuthController.java
@@ -1,11 +1,40 @@
 package com.rivals.rivalsapi.controller;
 
+import com.rivals.rivalsapi.dto.auth.AuthRequestDto;
+import com.rivals.rivalsapi.dto.auth.RegisterDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(path = "/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
+
+    @PostMapping(path = "/register")
+    public ResponseEntity<Object> register(
+            @RequestBody RegisterDto registerDto
+    ) {
+        try {
+            return ResponseEntity.ok("");
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping(path = "/login")
+    public ResponseEntity<Object> authenticate(
+            @RequestBody AuthRequestDto authDto
+    ) {
+        try {
+            return ResponseEntity.ok("");
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
 }
