de78f8297b5d29e7ceb7d3c0a90aa6dbafc05239
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 19:51:20 +0200

    feat: add authentication controller

---
+++ backend/src/main/java/com/rivals/rivalsapi/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.rivals.rivalsapi.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+}
