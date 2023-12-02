234c1b8f1e35ef8ca20de9f6e8fdf8a519416d35
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 16:38:30 +0200

    feat: add extract all claims method

--- backend/src/main/java/com/rivals/rivalsapi/service/JwtService.java
+++ backend/src/main/java/com/rivals/rivalsapi/service/JwtService.java
@@ -1,10 +1,28 @@
 package com.rivals.rivalsapi.service;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
     public String extractUsername(String token) {
+        return null;
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        // TODO: implement getSignInKey method
         return null;
     }
 }
