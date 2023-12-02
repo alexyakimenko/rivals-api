48ba717f105be6e8c8bdaf561fda2c2c255217f2
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-03 01:41:29 +0200

    feat: add user service & controller

---
+++ backend/src/main/java/com/rivals/rivalsapi/service/UserService.java
@@ -0,0 +1,12 @@
+package com.rivals.rivalsapi.service;
+
+import com.rivals.rivalsapi.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+}
