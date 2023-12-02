3ba36ea9832a840dda185aeace1bbbdbb2901083
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 17:03:38 +0200

    feat: add extract claim method

--- backend/src/main/java/com/rivals/rivalsapi/service/JwtService.java
+++ backend/src/main/java/com/rivals/rivalsapi/service/JwtService.java
@@ -7,12 +7,18 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "xXopuniMHtHJ6px5xmLcD9ebnf4dmZhW";
     public String extractUsername(String token) {
         return null;
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims); 
     }
 
     private Claims extractAllClaims(String token) {
