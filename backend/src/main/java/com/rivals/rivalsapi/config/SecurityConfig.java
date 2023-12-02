1780fd45ef5bafd0d0e1ada5ad4a08b65cce305a
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 20:45:50 +0200

    feat: add auth endpoints to whitelist

--- backend/src/main/java/com/rivals/rivalsapi/config/SecurityConfig.java
+++ backend/src/main/java/com/rivals/rivalsapi/config/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("").permitAll()
+                        .requestMatchers("/api/v1/auth/**").permitAll()
                         .anyRequest().authenticated())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
