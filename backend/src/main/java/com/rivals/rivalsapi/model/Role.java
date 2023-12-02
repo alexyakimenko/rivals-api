0dea1c404932c0b4fde935ff76b832b11af81698
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 20:31:36 +0200

    fix: private enum fix

---
+++ backend/src/main/java/com/rivals/rivalsapi/model/Role.java
@@ -0,0 +1,6 @@
+package com.rivals.rivalsapi.model;
+
+public enum Role {
+    USER,
+    ADMIN
+}
--- backend/src/main/java/com/rivals/rivalsapi/model/User.java
+++ backend/src/main/java/com/rivals/rivalsapi/model/User.java
@@ -13,10 +13,6 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Set;
 
-enum Role {
-    USER,
-    ADMIN
-}
 @Data
 @Builder
 @NoArgsConstructor
