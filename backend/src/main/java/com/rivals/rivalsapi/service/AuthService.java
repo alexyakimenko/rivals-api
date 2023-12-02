ec8f739fef8d753b1d295b82a3ed66ed8809f2d1
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 20:31:10 +0200

    feat: add register methond in authentication service

--- backend/src/main/java/com/rivals/rivalsapi/service/AuthService.java
+++ backend/src/main/java/com/rivals/rivalsapi/service/AuthService.java
@@ -3,15 +3,34 @@ package com.rivals.rivalsapi.service;
 import com.rivals.rivalsapi.dto.auth.AuthRequestDto;
 import com.rivals.rivalsapi.dto.auth.AuthResponseDto;
 import com.rivals.rivalsapi.dto.auth.RegisterDto;
+import com.rivals.rivalsapi.model.Role;
+import com.rivals.rivalsapi.model.User;
+import com.rivals.rivalsapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public AuthResponseDto register(RegisterDto registerDto) {
-        return null;
+        User user = User.builder()
+                .username(registerDto.getUsername())
+                .password(passwordEncoder.encode(registerDto.getPassword()))
+                .firstName(registerDto.getFirstName())
+                .lastName(registerDto.getLastName())
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+        String jwtToken = jwtService.generateToken(user);
+        return AuthResponseDto.builder()
+                .token(jwtToken)
+                .build();
     }
 
     public AuthResponseDto authenticate(AuthRequestDto authDto) {
