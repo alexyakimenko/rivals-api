1767eb3cd62596d47c49e294dceff53bbd964826
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 18:33:02 +0200

    feat: add generate token method

--- backend/src/main/java/com/rivals/rivalsapi/service/JwtService.java
+++ backend/src/main/java/com/rivals/rivalsapi/service/JwtService.java
@@ -2,18 +2,40 @@ package com.rivals.rivalsapi.service;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
 public class JwtService {
     private static final String SECRET_KEY = "xXopuniMHtHJ6px5xmLcD9ebnf4dmZhW";
+    private static final int EXPIRATION_TIME = 1000 * 60 * 24;
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
+    }
+    public String generateToken(UserDetails userDetails) {
+        return generateToken(new HashMap<>(), userDetails);
+    }
+    public String generateToken(
+            Map<String, Object> extraClaims,
+            UserDetails userDetails
+    ) {
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
     }
 
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
