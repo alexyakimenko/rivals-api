da59fa49c38217981cf56aef152121e3e95db29c
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-02 15:35:56 +0200

    feat: add user model

---
+++ backend/src/main/java/com/rivals/rivalsapi/model/User.java
@@ -0,0 +1,26 @@
+package com.rivals.rivalsapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+}
