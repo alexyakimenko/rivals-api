aecf1044af6c5619bb121830631e9b98912325a8
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-03 02:42:39 +0200

    feat: add user dto & mapper

---
+++ backend/src/main/java/com/rivals/rivalsapi/dto/user/UserDto.java
@@ -0,0 +1,27 @@
+package com.rivals.rivalsapi.dto.user;
+
+import com.rivals.rivalsapi.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+    private String username;
+    private String firstName;
+    private String lastName;
+
+    public static UserDto fromUser(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .build();
+    }
+}
