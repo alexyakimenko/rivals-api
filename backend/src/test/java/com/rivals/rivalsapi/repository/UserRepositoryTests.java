ed8c2682b437c1fc83b6bfaf021cbd7ba4d227a9
Alex Yakimenko <alexyakimenkojr@gmail.com>
2023-12-05 15:03:50 +0200

    feat: test custom find by username method

---
+++ backend/src/test/java/com/rivals/rivalsapi/repository/UserRepositoryTests.java
@@ -0,0 +1,43 @@
+package com.rivals.rivalsapi.repository;
+
+import com.rivals.rivalsapi.model.User;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
+@DataJpaTest
+public class UserRepositoryTests {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserRepositoryTests(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @AfterEach
+    void tearDown() {
+        userRepository.deleteAll();
+    }
+
+    @Test
+    void findByUsername_shouldFindUser() {
+        User user = User.builder()
+                .username("Alyekx")
+                .password("testpassword")
+                .firstName("Alexandr")
+                .build();
+        userRepository.save(user);
+        Optional<User> savedUser = userRepository.findByUsername("Alyekx");
+        Assertions.assertTrue(savedUser.isPresent());
+    }
+
+    @Test
+    void findByUsername_shouldNotFindUser() {
+        Optional<User> savedUser = userRepository.findByUsername("Alyekx");
+        Assertions.assertTrue(savedUser.isEmpty());
+    }
+}
